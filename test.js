ad54a744f105bde1f53aa6132c04cfef1fcd9bbc
Nerkolito <nerminkruskic@gmail.com>
2024-08-29 13:43:32 +0200

    Changes

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 if (condition) {
-    console.log("jävla skit git fitt")
-    console.log("Jag vill dö")
-    console.log("kan nikola ens git? l0l0l ")
-    console.log("varför uppdateras inte? ")
+  console.log("jävla skit git fitt");
+  console.log("Jag vill dö");
+  console.log("kan nikola ens git? l0l0l ");
+  console.log("varför uppdateras inte? ");
+  console.log("GIT MEISTER NIKO");
 }
