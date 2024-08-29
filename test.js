aafcb96b010fd022a188804c0186832ce56bac43
Atombombskatt <bjorn.samuelson96rgmail.com>
2024-08-29 13:40:55 +0200

    text

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
 if (condition) {
     console.log("jävla skit git fitt")
     console.log("Jag vill dö")
+    console.log("kan nikola ens git? l0l0l ")
+    console.log("varför uppdateras inte? ")
+}
